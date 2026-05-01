768422db00f4035ef2fe9bdfb597e8d9c8e6f00e
K-Joshua <saintangel9989@gmail.com>
2026-05-01 08:49:48 +0800

    Refactor: Added Role Validation on Extracting Document Series

--- Backend/Backend/Backend/Helper/ExtractDocuSer.cs
+++ Backend/Backend/Backend/Helper/ExtractDocuSer.cs
@@ -8,15 +8,19 @@ namespace Backend.Backend.Helper
         /// Extract The Id from Document Series
         /// </summary>
         /// <param name="DocumentSeries"></param>
+        /// <param name="position"></param>
         /// <returns></returns>
-        public static (int ExtractedId, int statusCode) GetIdByExtractingDocumentSeries(this string DocumentSeries)
+        public static (int ExtractedId, int statusCode) ExtractDataFromDocumentSeries(this int position, string DocumentSeries)
         {
+            if (position <= 0 || position > 3)
+                return (0, 401); // Unathorized
+
             string[] Data = DocumentSeries.Split('-');
 
-            if (string.IsNullOrEmpty(Data[2]))
-                return (0, 404);
+            if (string.IsNullOrEmpty(Data[position - 1]))
+                return (0, 404); // Null or Empty ID
 
-            if (int.TryParse(Data[2], out int Id))
+            if (int.TryParse(Data[position - 1], out int Id)) // Extract
                 return (Id, 200);
 
             return (0, 422);
