c107fce836c693ecff8edd8cac2d46a98330b915
K-Joshua <saintangel9989@gmail.com>
2026-05-02 10:47:53 +0800

    Feat: Added Login to Auth and Updated User's Registration with additional token as return

--- Backend/Backend/Backend/Service/AuthService.cs
+++ Backend/Backend/Backend/Service/AuthService.cs
@@ -1,8 +1,13 @@
 ﻿using Backend.Backend.Configuration;
 using Backend.Backend.DTOs;
+using Backend.Backend.Interface.ConfigureInterface;
+using Backend.Backend.Interface.RepositoryInterface;
 using Backend.Backend.Interface.ServiceInterface;
 using Backend.Backend.Repository;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+using Backend.Backend.Model;
 
 namespace Backend.Backend.Service
 {
@@ -13,22 +18,27 @@ namespace Backend.Backend.Service
     /// This service validates user credentials, retrieves claims, 
     /// and generates a JWT token upon successful authentication.
     /// </remarks>
-    public class AuthService(UserRepository userRepository, ClaimService claimService, JwtService jwtService) : IAuthService // for primary constructor, will practice more on primary constructor as per release of C# 12
+    /// 
+
+    public class AuthService(IUserRepository userRepository, IClaimService claimService, IJwtService jwtService, UserManager<User> userManager) : IAuthService // for primary constructor, will practice more on primary constructor as per release of C# 12
     {
         /// <summary>
         /// Repository used to access user data from the database.
         /// </summary>
-        private readonly UserRepository _userRepository  = userRepository;
+        private readonly IUserRepository _userRepository  = userRepository;
 
         /// <summary>
         /// Service used to generate claims for the authenticated user.
         /// </summary>
-        private readonly ClaimService _claimService = claimService;
+        private readonly IClaimService _claimService = claimService;
 
         /// <summary>
         /// Service used to generate JWT tokens.
         /// </summary>
-        private readonly JwtService _jwtService = jwtService;
+        private readonly IJwtService _jwtService = jwtService;
+
+        // Since ASP Net already gave us User Manager, lets use the Identity System
+        private readonly UserManager<User> _userManager = userManager;
 
         /// <summary>
         /// Authenticates a user using email/username and password.
@@ -51,17 +61,27 @@ namespace Backend.Backend.Service
         /// </remarks>
         public async Task<LoginResult> LoginAsync(LoginUserDto login)
         {
-            var user = await _userRepository.GetByEmailOrUsernameAsync(login.Email);
+
+            // method = find user by email
+            var user = await _userManager.FindByEmailAsync(login.Email);
+
+            // fallback = find by username
+            if (user == null)
+            {
+                user = await _userManager.FindByNameAsync(login.Email);
+            }
 
             // User Validation
             if (user == null) return new LoginResult
             {
                 isSuccess = false,
                 Token = null,
-                Detail = "User Cannot Be Found"
+                Detail = "Email Incorrect   "
             };
+
+            var passValidation = await _userManager.CheckPasswordAsync(user, login.Password);
             // Password validation
-            if (!BCrypt.Net.BCrypt.Verify(login.Password, user.PassHash))
+            if (!passValidation)
                 return new LoginResult
                 {
                     isSuccess = false,
@@ -72,7 +92,9 @@ namespace Backend.Backend.Service
             // collect user's identity
             var claims = await _claimService.GetClaimsAsync(user);
 
+            // generate token
             var token = _jwtService.GenerateToken(claims);
+
 
             return new LoginResult
             {
