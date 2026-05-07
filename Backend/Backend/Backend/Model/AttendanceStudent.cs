af322d2acd6bcecb5c32391bc08505e8c11379c8
K-Joshua <saintangel9989@gmail.com>
2026-05-07 14:26:30 +0800

    Feat: Added New Model For New Logic Block Section

---
+++ Backend/Backend/Backend/Model/AttendanceStudent.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using AttStatus = Backend.Backend.Helper.Enum.AttendanceEnum.AttStatus;
+
+namespace Backend.Backend.Model
+{
+    public class AttendanceStudent
+    {
+        [Required]
+        public required int Attendance_Id { get; set; }
+        public required Attendance Attendance { get; set; }
+
+        [Required]
+        public required int Student_Id { get; set; }
+        public required Student Student { get; set; }
+
+        [Required]
+        public required AttStatus StudentAttendance { get; set; } = AttStatus.Absent;
+    }
+}
