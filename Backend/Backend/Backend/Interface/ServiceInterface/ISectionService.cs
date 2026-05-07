2d8bc1b11fabe588663f94faab0524b9934063ca
K-Joshua <saintangel9989@gmail.com>
2026-05-07 14:42:26 +0800

    Fix: Fix Interface to DTO Redirection

--- Backend/Backend/Backend/Interface/ServiceInterface/ISectionService.cs
+++ Backend/Backend/Backend/Interface/ServiceInterface/ISectionService.cs
@@ -5,7 +5,7 @@ namespace Backend.Backend.Interface.ServiceInterface
     public interface ISectionService
     {
         Task<ResponseDTO<IEnumerable<GetSectionDTO>>> GetAllAsync();
-        Task<ResponseDTO<GetSectiontDTO>> GetByIdAsync(int id);
+        Task<ResponseDTO<GetSectionDTO>> GetByIdAsync(int id);
         Task<ResponseDTO<GetSectionDTO>> AddAsync(AddSectionDTO dto);
         Task<ResponseDTO<GetSectionDTO>> UpdateAsync(int id, AddSectionDTO dto);
         Task<bool> DeleteAsync(int id);
