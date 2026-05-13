0cbbd1370df2500e9edbf5c74b077fa1bd52c99d
K-Joshua <saintangel9989@gmail.com>
2026-05-13 10:20:06 +0800

    Feat: Addde New Qr Code Operation

--- Backend/Backend/Backend/Interface/ServiceInterface/IAttendanceStudentService.cs
+++ Backend/Backend/Backend/Interface/ServiceInterface/IAttendanceStudentService.cs
@@ -6,7 +6,7 @@ namespace Backend.Backend.Interface.ServiceInterface
     {
         Task<ResponseDTO<IEnumerable<GetAttendanceStudentDTO>>> GetAllAsync();
         //Task<ResponseDTO<GetAttendanceStudentDTO>> GetByIdAsync(int id);
-        Task<ResponseDTO<GetAttendanceStudentDTO>> AddAsync(string uuid);
+        Task<ResponseDTO<GetAttendanceStudentDTO>> AddAsync(ScanRequest request);
         //Task<ResponseDTO<GetAttendanceStudentDTO>> UpdateAsync(int id, AddAttendanceStudentDTO dto);
         //Task<bool> DeleteAsync(int id);
     }
