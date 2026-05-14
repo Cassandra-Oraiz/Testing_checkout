98f8d8c596a3196dc7e8011f1c8a31000f24c011
K-Joshua <saintangel9989@gmail.com>
2026-05-14 17:30:16 +0800

    Feat: Removed AddDto with claims

--- Backend/Backend/Backend/Interface/RepositoryInterface/IScheduleRepository.cs
+++ Backend/Backend/Backend/Interface/RepositoryInterface/IScheduleRepository.cs
@@ -7,6 +7,7 @@ namespace Backend.Backend.Interface.RepositoryInterface
         Task<IEnumerable<Schedule>> GetAllAsync();
         Task<Schedule?> GetByIdAsync(int id);
         Task AddAsync(Schedule schedule);
+        Task<Schedule?> GetScheduleIfExist(string id, DayOfWeek dayOfWeek, TimeOnly now);
         Task<bool> HasConflictingScheduleAsync(int courseId, string academicYear, TimeOnly startTime, TimeOnly endTime, int sectionId);
         Task UpdateAsync(Schedule schedule);
         Task DeleteAsync(Schedule schedule);
--- Backend/Backend/Backend/Interface/ServiceInterface/IAttendanceService.cs
+++ Backend/Backend/Backend/Interface/ServiceInterface/IAttendanceService.cs
@@ -6,8 +6,8 @@ namespace Backend.Backend.Interface.ServiceInterface
     {
         Task<ResponseDTO<IEnumerable<GetAttendanceDTO>>> GetAllAsync();
         Task<ResponseDTO<GetAttendanceDTO>> GetByIdAsync(int id);
-        Task<ResponseDTO<GetAttendanceDTO>> AddAsync(AddAttendanceDTO dto, string currentUserId);
-        Task<ResponseDTO<GetAttendanceDTO>> UpdateAsync(int id, AddAttendanceDTO dto);
+        Task<ResponseDTO<GetAttendanceDTO>> AddAsync(string currentUserId);
+        Task<ResponseDTO<GetAttendanceDTO>> UpdateAsync(int id);
         Task<bool> DeleteAsync(int id);
     }
 }
